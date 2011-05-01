081e08db4bef2987a200d6e7a79846e8b6cc01ff
Phally <info@frankdegraaf.net>
2011-05-01 13:16:58 +0200

    Made a processor instead of a filter.

--- libs/asset_compress/less.php
+++ libs/asset_compress/less.php
@@ -1,5 +1,5 @@
 <?php
-App::import('Model', 'AssetCompress.AssetFilterInterface');
+App::import('Model', 'AssetCompress.AssetProcessorInterface');
 App::import('Vendor', 'Less.lessphp/lessc', array('file' => 'lessphp/lessc.inc.php'));
 
 /**
@@ -8,16 +8,20 @@ App::import('Vendor', 'Less.lessphp/lessc', array('file' => 'lessphp/lessc.inc.p
  * @author Frank de Graaf (Phally)
  * @license MIT
  */
-final class LessFilter implements AssetFilterInterface {
+final class LessProcessor implements AssetProcessorInterface {
 
 	/**
 	 * Parses LESS css code.
 	 *
+	 * @param string $fileName The filename of the file.
 	 * @param string $content content of *.css files.
 	 * @return string
 	 * @access public
 	 */
-	public function filter($content) {
+	public function process($fileName, $content) {
+		if (substr($fileName, -9) != '.less.css') {
+			return $content;
+		}
 		$lessc = new lessc();
 		return $lessc->parse($content);
 	}
