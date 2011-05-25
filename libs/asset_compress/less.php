a5666c001513363b760beaa4a94180246a3e100c
Phally <info@frankdegraaf.net>
2011-05-25 21:50:51 +0200

    Support for user controlled LessPHP versions.

--- libs/asset_compress/less.php
+++ libs/asset_compress/less.php
@@ -1,6 +1,5 @@
 <?php
 App::import('Model', 'AssetCompress.AssetProcessorInterface');
-App::import('Vendor', 'Less.lessphp/lessc', array('file' => 'lessphp/lessc.inc.php'));
 
 /**
  * LessProcessor for Mark Story's Asset Compress plugin.
@@ -25,6 +24,11 @@ class LessProcessor implements AssetProcessorInterface {
 	 * @return string
 	 */
 	public function process($fileName, $content) {
+		$path = 'lessphp/lessc';
+		$options = array('file' => 'lessphp/lessc.inc.php');
+		if (!App::import('Vendor', $path, $options)) {
+			App::import('Vendor', 'Less.' . $path, $options);
+		}
 		foreach ($this->_extensions as $extension) {
 			if (strtolower(substr($fileName, -strlen($extension))) == $extension) {
 				$lessc = new lessc();
